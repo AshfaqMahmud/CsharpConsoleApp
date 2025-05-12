82bc3f0c619216a2fe87783df955c465020039b3
Ashfaq Mahmud <ash.mse21@gmail.com>
2025-05-13 01:11:30 +0600

    linkedlist

---
+++ CsharpConsoleApp/LinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConsoleApp
+{
+    public class Node
+    {
+        public int Data;
+        public Node? Next;
+
+        public Node(int Data) 
+        {
+            this.Data = Data;
+            Next = null;
+        }
+    }
+
+    internal class LinkedList
+    {
+        public Node? Head;
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void Append(int data)
+        {
+            Node newnode = new Node(data);
+            if (Head == null)
+            {
+                Head = newnode;
+            }
+            else
+            {
+                Node temp = Head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newnode;
+            }
+        }
+
+        public void Prepend(int data)
+        {
+            Node newnode = new Node(data);
+            newnode.Next = Head;
+            Head = newnode;
+        }
+
+        public void Delete(int key)
+        {
+            if (Head == null) return;
+            if (Head.Data == key)
+            {
+                Head = Head.Next;
+                return;
+            }
+            Node temp = Head;
+            while (temp.Next != null && temp.Next.Data != key)
+            {
+                temp = temp.Next;
+            }
+
+            // if data was not found
+            if(temp.Next == null) return;
+
+            // if data was found and not meet the previous condition
+            temp.Next = temp.Next.Next;
+        }
+
+        public void Print()
+        {
+            Node temp = Head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+        public bool isThere(int key)
+        {
+            Node temp = Head;
+            while (temp != null)
+            {
+                if (temp.Data == key) return true;
+                temp = temp.Next;
+            }
+            return false;
+        }
+    }
+}
--- CsharpConsoleApp/Program.cs
+++ CsharpConsoleApp/Program.cs
@@ -6,19 +6,31 @@ namespace CsharpConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("\n");
 
             int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
 
             //Sort.BubbleSort(arr);
             Sort.QuickSort(arr, 0, arr.Length - 1);
 
-            Console.WriteLine("Sorted array");
+            Console.WriteLine("Sorted array:");
             for(int i = 0; i< arr.Length; i++)
             {
                 Console.Write(arr[i] + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
 
+            // Linked List Example
+            LinkedList list = new LinkedList();
+            int listSize = 5;
+            Random rand = new Random();
+            for (int i = 0; i < listSize; i++)
+            {
+                list.Append(rand.Next() % 20);
+            }
+
+            list.Print();
+            Console.WriteLine(list.isThere(5));
         }
     }
 }
